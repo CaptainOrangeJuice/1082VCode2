8f294a075badbf31d13736c23d069ac4539938dc
Captain Orange Juice <bacespaby@gmail.com>
2024-11-23 16:31:57 -0600

    Changed PID.h so that it would build and changed the font to Papyrus because I hate myself

--- include/PID.h
+++ include/PID.h
@@ -25,68 +25,13 @@ class PID
     
 
 public:
-    PID() {
-    }
+    PID();
 
-    void reset()
-    {
-        error = 0;
-        prev = 0;
-        i = 0;
-        Left.resetPosition();
-        Right.resetPosition();
-    }
+    void reset();
 
-    void update()
-    {
-        position = ((abs(Tl.position(vex::turns)) + abs(Tr.position(vex::turns))) / 2.0) * M_PI * 3.25;
-        error = target - position;
+    void update();
 
-        if (error = prev) {
-            printToConsole("The error is not changing. PID stopping.");
-            Brain.Screen.print("The error is not changing. PID stopping.");
-            errorChanging = false;
-        } 
+    bool isStopped();
 
-        i = i + error + (prev - error) / 2.0;
-        d = error - prev;
-        prev = error;
-        if (error == 0)
-        {
-            i = 0;
-        }
-
-        if (fabs(i) >= 100)
-        {
-            i = (i / fabs(i)) * 100;
-        }
-
-        
-    }
-
-    bool isStopped() {
-        if ((Left.velocity(vex::rpm) + Right.velocity(vex::rpm))/2 <= 1 || ((error == prev) && (Left.velocity(vex::rpm) + Right.velocity(vex::rpm))/2 <= 1)) return true;
-        else return false;
-    }
-
-    void runPID(double targetVal, double timeLimit)
-    {
-        reset();
-        target = targetVal;
-        while (abs(position - target) > 0.2 && errorChanging) {
-            update();
-            //spinAll(true, (kp * error) + (ki * i) + (kd * d));
-            Left.spin(vex::forward, (kp * error) + (ki * i) + (kd * d), vex::pct);
-            Right.spin(vex::forward, (kp * error) + (ki * i) + (kd * d), vex::pct);
-
-            if (isStopped()) { break; }
-            _time += 20;
-            vex::wait(20, vex::msec);
-            if (_time >= timeLimit * 1000) {
-                break;
-            }
-        }
-
-        vex::wait(20, vex::msec);
-    }
+    void runPID(double targetVal, double timeLimit);
 };
